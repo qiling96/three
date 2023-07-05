e327bdb3eb03b3f0a7474d686efa66c9d749440e
qiling <qiling@qunhemail.com>
2023-07-06 00:08:42 +0800

    feat:day2

--- src/main/main.tsx
+++ src/main/main.tsx
@@ -2,16 +2,19 @@
  * @Author: Qiling
  * @Date: 2023-07-04 23:51:39
  * @LastEditors: qiling qiling@qunhemail.com
- * @LastEditTime: 2023-07-05 01:25:23
- * @FilePath: \01-three_basic\src\main\main.tsx
+ * @LastEditTime: 2023-07-06 00:03:17
+ * @FilePath: \three\src\main\main.tsx
  * @Description:
  *
  */
 import * as THREE from "three";
 // 导入轨道控制器
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-// console.log(THREE);
-// 目的：设置物体缩放
+// 导入动画库
+import { gsap } from "gsap";
+// 导入dat.gui
+import * as dat from "dat.gui";
+
 // 初始化场景
 const scene = new THREE.Scene();
 
@@ -40,45 +43,99 @@ const edgesMaterial = new THREE.LineBasicMaterial({
   color: 0x000000,
 });
 var line = new THREE.LineSegments(edges, edgesMaterial);
-// 网格模型和网格模型对应的轮廓线框插入到场景中
+const group = new THREE.Group();
+group.add(cube);
+group.position.y = 5;
+scene.add(group);
 
-// 修改物体位置
+const gui = new dat.GUI();
+gui.add(group.position, "x").min(0).max(5).step(0.1);
+const parmas = {
+  handleClick: () => {
+    gsap.to(group.position, {
+      x: 3,
+      duration: 2,
+      ease: "bounce.in",
+      repeat: -1,
+    });
+  },
+};
 
-// 缩放
-// cube.scale.set(1, 2, 3);
-// line.scale.set(1, 2, 3);
-// 旋转
-// 弧度
-cube.rotation.set(0.5, 0.5, 0.5);
-line.rotation.set(0.5, 0.5, 0.5);
-scene.add(cube, line);
+gui
+  .addColor({ color: 0xffff00 }, "color")
+  .name("颜色")
+  .onChange((color) => {
+    cube.material.color.set(color);
+  });
+gui.add(group, "visible").name("是否可见");
+// 点击触发某个事件
+gui.add(parmas, "handleClick").name("点击事件");
+
+const folder=gui.addFolder("设置立方体");
+folder.add(cube.material,"wireframe").name("是否显示线框");
 // 初始化渲染器
-const render = new THREE.WebGLRenderer({ antialias: true });
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 // 设施渲染尺寸大小
-render.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(window.innerWidth, window.innerHeight);
 // 将webgl中的canvas添加到body
-document.body.appendChild(render.domElement);
+document.body.appendChild(renderer.domElement);
 // 使用渲染器，通过相机将场景渲染到屏幕上
 
-render.render(scene, camera);
+renderer.render(scene, camera);
 
 // 创建轨道控制器
-const controls = new OrbitControls(camera, render.domElement);
+const controls = new OrbitControls(camera, renderer.domElement);
+// 设置控制器阻尼，更真实，必须在动画循环里调用update
+controls.enableDamping = true;
 
 // 添加坐标轴辅助器
 const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
-function handleRender() {
-  cube.position.x += 0.01;
-  cube.rotation.x += 0.01;
-  line.position.x += 0.01;
-  line.rotation.x += 0.01;
-  if (cube.position.x > 5) {
-    cube.position.x = 0;
-    line.position.x = 0;
+// 设置时钟
+const clock = new THREE.Clock();
+
+// 设置动画
+const animate1 = gsap.to(group.position, {
+  y: 0,
+  duration: 5,
+  ease: "bounce.out",
+  repeat: -1,
+});
+// 控制动画播放和暂停
+window.addEventListener("dblclick", () => {
+  if (animate1.isActive()) {
+    animate1.pause();
+  } else {
+    animate1.resume();
   }
-  render.render(scene, camera);
+});
+// 控制全屏和退出
+window.addEventListener("keydown", (e) => {
+  if (e.key === "f") {
+    if (document.fullscreenElement) {
+      document.exitFullscreen();
+    } else {
+      document.body.requestFullscreen();
+    }
+  }
+});
+
+gsap.to(group.rotation, { x: Math.PI * 2, duration: 5, ease: "bounce.out" });
+function render() {
+  controls.update();
+  renderer.render(scene, camera);
   //   请求下一帧渲染
-  requestAnimationFrame(handleRender);
+  requestAnimationFrame(render);
 }
-handleRender();
+render();
+// 监听画面变化，更新渲染画面
+window.addEventListener("resize", () => {
+  // 更新渲染器尺寸
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  // 更新渲染器像素比
+  renderer.setPixelRatio(window.devicePixelRatio);
+  // 更新摄像头
+  camera.aspect = window.innerWidth / window.innerHeight;
+  // 更新摄像机投影矩阵
+  camera.updateProjectionMatrix();
+});
